978941da28bab6878aae0a82b9195bb269ede599
Sandy Bassem <sandybassem445@gmail.com>
2026-09-15 12:49:28 +0300

    updating car installment calculator

--- home.js
+++ home.js
@@ -8,24 +8,24 @@ document.getElementById("InstallmentPeriod").addEventListener('keyup',Calculator
 function Interestrate(years){
     var Interestrate=0;
     if(years==1){
-       Interestrate= 0.01
+       Interestrate= 0.1
     }else if(years==2){
-         Interestrate= 0.018
+         Interestrate= 0.18
 
     } else if(years==3){
-         Interestrate= 0.029
+         Interestrate= 0.29
 
     }else if(years==4){
-         Interestrate= 0.038
+         Interestrate= 0.38
 
     }else if(years==5){
-         Interestrate= 0.049
+         Interestrate= 0.49
 
     }else if(years==6){
-         Interestrate= 0.062
+         Interestrate= 0.62
 
     }else if(years==7){
-         Interestrate= 0.079
+         Interestrate= 0.79
 
     }
 return  Interestrate
@@ -65,16 +65,14 @@ var years= Number (document.getElementById("InstallmentPeriod").value);
 
     if( price==0||DownPayment==0||years==0){
     document.querySelector("#MonthelyInstallment").innerText="Please fill in all required fields";
- document.querySelector("#InstallmentRate").innerText="Please fill in all required fields";
- document.querySelector('#InstallmentRate').style.color = '#b02a37';
+ document.querySelector("#InstallmentRate").innerText="";
     document.querySelector('#MonthelyInstallment').style.color = '#b02a37';
 
 
 }else if(years>7){
      document.querySelector("#MonthelyInstallment").innerText="Installment period cannot exceed 7 years.";
-    document.querySelector("#InstallmentRate").innerText="Please enter an installment period between 1 and 7 years.";
-     document.querySelector('#InstallmentRate').style.color = '#b02a37';
-      document.querySelector('#MonthelyInstallment').style.color = '#b02a37';
+    document.querySelector("#InstallmentRate").innerText="";
+     document.querySelector('#MonthelyInstallment').style.color = '#b02a37';
 
 }else{
 var Remaining=RemainingValue( price,DownPayment)
@@ -83,8 +81,8 @@ var month = period(years);
 var TotalInstallements = Interest+Remaining;
  var MonthlyInstallment= TotalInstallements/month; 
  
-document.querySelector("#MonthelyInstallment").innerText=MonthlyInstallment.toFixed(2);
-document.querySelector("#InstallmentRate").innerText= (Interestrate(years)*100).toFixed(2)+"%";
+document.querySelector("#MonthelyInstallment").innerText=MonthlyInstallment.toFixed(2)+" EGP";
+document.querySelector("#InstallmentRate").innerText= " Installment Rate: "+(Interestrate(years) * 100).toFixed(0)+"%";
  document.querySelector("#InstallmentRate").style.color = "";
 document.querySelector("#MonthelyInstallment").style.color = "";
 }
